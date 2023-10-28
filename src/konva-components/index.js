08f76be6d45a8cd48af797524f00aa091dce5cb9
LG <34185919+projektorius96@users.noreply.github.com>
2023-10-28 22:59:36 +0100

    working draft, see desc.
    - just like Konva.js docs recommended, don't use sceneFunc for any of side-effects, then I though I could manipulate DataImage using Konva.Image as some sort of broker, so I've tried it and it just worked !

--- src/konva-components/index.js
+++ src/konva-components/index.js
@@ -3,10 +3,12 @@ import { Layer } from 'konva/lib/Layer';
 import { Group } from 'konva/lib/Group';
 import { Node } from 'konva/lib/Node';
 import { Shape } from 'konva/lib/Shape';
+import { Image } from 'konva/lib/shapes/Image';
 export { 
     Stage, 
     Layer, 
     Group, 
     Node, 
     Shape,
+    Image as KonvaImage
 }
